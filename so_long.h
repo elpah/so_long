411036937a002b7b6d0802776a4b32962e9bc5c3
Elpah <obengelpachris@gmail.com>
2024-11-03 00:20:15 +0100

    include header files

--- so_long.h
+++ so_long.h
@@ -6,12 +6,40 @@
 /*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:21:14 by eobeng            #+#    #+#             */
-/*   Updated: 2024/11/03 00:17:04 by elpah            ###   ########.fr       */
+/*   Updated: 2024/11/03 00:20:02 by elpah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 #define SO_LONG_H
+
+// # include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "./gnl/get_next_line.h"
+#include "./libft/libft.h"
+#include "./printf/ft_printf.h"
+// # include <mlx.h>
+
+#define SIZE 80
+#define TEXTURE_WALL "textures/xpm/wall.xpm"
+#define TEXTURE_FLOOR "textures/xpm/floor.xpm"
+#define TEXTURE_CHARACTER_LEFT "textures/xpm/wiz_left.xpm"
+#define TEXTURE_CHARACTER_RIGHT "textures/xpm/wiz_right.xpm"
+#define TEXTURE_DOOR_OPEN "textures/xpm/door2.xpm"
+#define TEXTURE_DOOR_CLOSE "textures/xpm/door1.xpm"
+#define TEXTURE_COLLECTIBLE "textures/xpm/herb.xpm"
+
+#define BUFFER_SIZE 1024
+
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
+#define KEY_ESC 65307
 
 typedef struct s_game
 {
