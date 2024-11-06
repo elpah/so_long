1d88ab987e38c67ad060e7f17dc68ea600edd943
Elpah <obengelpachris@gmail.com>
2024-11-06 01:30:22 +0100

    function to set initial values of map struct

---
+++ src/init_map.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 23:57:46 by eobeng            #+#    #+#             */
+/*   Updated: 2024/11/06 01:29:59 by elpah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	init_game_variables(t_game *game)
+{
+	game->map = NULL;
+	game->player_x = 0;
+	game->player_y = 0;
+	game->direction = 'N';
+	game->last_pos = '0';
+	game->steps = 0;
+	game->gate = 0;
+	game->img_wh = 64;
+	game->wiz_img = NULL;
+	game->door[0] = NULL;
+	game->door[1] = NULL;
+	game->character[0] = NULL;
+	game->character[1] = NULL;
+	game->floor = NULL;
+	game->wall = NULL;
+	game->treasure = NULL;
+	game->addr = NULL;
+	game->collectibles = 0;
+	game->collectibles_count = 0;
+}
